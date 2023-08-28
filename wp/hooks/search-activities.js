1af40203be6330615d4d5581bd968f470ef34676
Francesc Busquets <francesc@gmail.com>
2023-08-28 12:58:32 +0200

    Update WP search hook

--- wp/hooks/search-activities.js
+++ wp/hooks/search-activities.js
@@ -3,22 +3,26 @@
 
 jq(document).ready(async function () {
 
+  const SEARCH_API = 'https://clic.xtec.cat/db/repo-search';
+  const PROJECTS_ROOT = 'https://clic.xtec.cat/projects';
+  const SITE_ROOT = 'https://projectes.xtec.cat/clic';
+
   // Performs an API query looking for activities with the provided expression
   async function searchActivities(lang, query) {
     const results = [];
-    const matches = await fetch(`https://clic.xtec.cat/db/repo-search/?q=${encodeURIComponent(query)}&lang=${lang}&method=boolean`)
+    const matches = await fetch(`${SEARCH_API}/?q=${encodeURIComponent(query)}&lang=${lang}&method=boolean`)
       .then(res => res.json())
       .catch(err => console.error(`Error searching "${query}" in activities with lang "${lang}": ${err}`));
     if (matches?.length) {
-      const projects = await fetch('https://clic.xtec.cat/projects/projects.json')
+      const projects = await fetch(`${PROJECTS_ROOT}/projects.json`)
         .then(res => res.json())
         .catch(err => console.log(`Error retrieving projects list: ${err}`));
       matches.forEach(match => {
         const project = projects.find(p => p.path === match);
         if (project) {
           const { title, author, cover, coverWebp, langCodes } = project;
-          const link = `https://projectes.xtec.cat/clic/${lang}/repo/?prj=${match}`;
-          const img = `https://clic.xtec.cat/projects/${match}/${coverWebp || cover}`;
+          const link = `${SITE_ROOT}/${lang}/repo/?prj=${match}`;
+          const img = `${PROJECTS_ROOT}/${match}/${coverWebp || cover}`;
           results.push({ title, author, img, link, match, langCodes });
         }
       });
